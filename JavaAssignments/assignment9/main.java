4d8a33b3e243ee384a1c32d803d8bb0b6d78f594
sreejithack <cksreejitha@gmail.com>
2016-12-20 20:12:51 +0530

    main

---
+++ JavaAssignments/assignment9/main.java
@@ -0,0 +1,19 @@
+package first;
+
+public class main   {
+ 
+	
+	
+	
+	public static void main(String[] args) {
+		
+		PermananetEmployee pemp=new PermananetEmployee("sam", "HR", 29, 200, 300, 5000);
+		pemp.setpcode(1001);
+		pemp.disp();
+		pemp.empdisplay();
+		
+		
+
+	}
+
+}
