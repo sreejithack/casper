b3f5ddb637e6735cd7c147544cfb7d9123118aef
sreejithack <cksreejitha@gmail.com>
2016-12-20 20:27:47 +0530

    Assignment10

---
+++ JavaAssignments/assignment10/Contractmain.java
@@ -0,0 +1,13 @@
+package first;
+
+public class Contractmain {
+
+	public static void main(String[] args) {
+		Contractemployee cemp=new Contractemployee("Ram", "Accountant", 29, 500, 600, 7000);
+		cemp.setduration(5);
+		cemp.Getduration();
+		cemp.displ();
+
+	}
+
+}
