5e00720547b944d3c5b8263136b9dd6b82c2fe9e
sreejithack <cksreejitha@gmail.com>
2016-12-21 12:52:25 +0530

    assignment10

---
+++ JavaAssignments/Assignment11/Employee.java
@@ -0,0 +1,41 @@
+package first;
+
+public class Employee {
+
+	String name;
+	String designation;
+	int age,PF,Gratuity,salary;
+	public Employee(String name,String designation,int age,int PF,int Gratuity,int salary)
+	{
+		this.name=name;
+		this.designation=designation;
+		this.age=age;
+		this.PF=PF;
+		this.Gratuity=Gratuity;
+		this.salary=salary;
+		
+	}
+	
+		public int calculatesalary()
+		{
+			return(salary-(PF+Gratuity));
+		}
+ void empdisplay()
+ {
+	 System.out.println("Name:"+name);
+		//System.out.println("Designation:"+designation);
+		System.out.println("Age:"+age);
+		System.out.println("Salary:"+salary);
+		
+		//System.out.println("TotalSalary:"+calculatesalary());
+		
+ }
+	
+	
+	
+		
+	
+	
+	
+
+}
---
+++ JavaAssignments/Assignment11/main.java
@@ -0,0 +1,25 @@
+package first;
+
+public class main   {
+ 
+	
+	
+	
+	public static void main(String[] args) {
+		
+		PermananetEmployee pemp=new PermananetEmployee("John", "HR", 35, 200, 300, 15000);
+		pemp.empdisplay();
+		pemp.setpcode("PM100");
+		pemp.Setphone(98967623);
+		pemp.SetEmpcode(1001);
+		pemp.setgender("Male");
+		pemp.disp();
+		
+		
+		
+		
+		
+
+	}
+
+}
