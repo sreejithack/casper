73494c192deb4d5004022ca265c45a2226c1484e
sreejithack <cksreejitha@gmail.com>
2016-12-28 11:07:30 +0530

    Assignment18

---
+++ JavaAssignments/Assignment18/Contractemployee.java
@@ -0,0 +1,100 @@
+package first;
+
+public class Contractemployee  extends Employee implements promotinalOffers{
+
+	public Contractemployee(String name, String designation, int age, int PF,
+			int Gratuity, int salary) {
+		super(name, designation, age, PF, Gratuity, salary);
+		
+	}
+	int ContractDuration;
+	public void setduration(int duration)
+	{
+		this.ContractDuration=duration;
+	}
+	public int Getduration()
+	{
+		return  ContractDuration;
+	}
+	
+	int Empcode;
+	public void SetEmpcode(int Ecode)
+	{
+		this.Empcode=Ecode;
+	}
+	public int GetEmpcode()
+	{
+		return  Empcode;
+	}
+	int phone;
+	public void Setphone(int ph)
+	{
+		this.phone=ph;
+	}
+	public int Getphone()
+	{
+		return  phone;
+	}
+	String Gender;
+	public void setgender(String gender)
+	{
+		this.Gender=gender;
+	}
+	
+	public String Getgender()
+	{
+		return  Gender;
+	}
+	public void SetTotalEmi(int TotalEmi)
+	{
+		this.totalEMI=TotalEmi;
+	}
+	public int GetTotalEmi()
+	{
+		return  totalEMI;
+	}
+	
+	void disp()
+	{
+		System.out.println("EmpCode:"+ GetEmpcode());
+		System.out.println("Gender:"+ Getgender());
+		System.out.println("PhoneNo:"+ Getphone());
+		System.out.println("ContractDuration:"+Getduration());
+	}
+	
+	public void seasonOffer(int totalPurchaseAmount) {
+		
+		totalPurchaseAmount=totalPurchaseAmount*15/100;
+		System.out.println("15% of purchase:"+totalPurchaseAmount);
+	}
+	
+	public void regularOffer(int amount) {
+		
+		amount=amount*8/100;
+		System.out.println("8% of purchase:"+amount);
+	}
+	void registerInsurance()
+	{
+		InsuranceNo="INS_"+GetEmpcode()+"111";
+		System.out.println("InsuranceNo:"+InsuranceNo);
+		System.out.println("InsureAmount:"+GetInsureAmount());
+		
+	}
+	void payEmi()
+	{ int emi;
+		emi=GetInsureAmount()*8/100;
+		int PayEMI=emi+GetTotalEmi();
+		System.out.println("PayEMI:"+PayEMI);
+		
+	}
+}
+	
+			
+		
+		
+			
+
+
+	
+
+
---
+++ JavaAssignments/Assignment18/Employee.java
@@ -0,0 +1,116 @@
+package first;
+
+public class Employee extends Insurance{
+
+	String name;
+	String designation;
+	int age,PF,Gratuity,salary;
+	public Employee(String name,String designation,int age,int PF,int Gratuity,int salary)
+	{
+		this.name=name;
+		this.designation=designation;
+		this.age=age;
+		this.PF=PF;
+		this.Gratuity=Gratuity;
+		this.salary=salary;
+		
+	}
+	
+		public int calculatesalary()
+		{
+			return(salary-(PF+Gratuity));
+		}
+		
+ void empdisplay()
+ {
+	 System.out.println("Name:"+name);
+		////System.out.println("Designation:"+designation);
+		System.out.println("Age:"+age);
+		//System.out.println("Salary:"+salary);
+		
+		System.out.println("TotalSalary:"+calculatesalary());
+		
+ }
+
+
+void registerInsurance() {
+	
+
+	InsuranceNo="INS_"+getecode()+"670";
+	
+System.out.println("InsuranceNo:"+InsuranceNo);
+	System.out.println("insureAmount:"+GetInsureAmount());
+	
+}
+
+
+void payEMI() {
+	System.out.println("EMI:"+getemi());
+	
+}
+	
+int claimInsurance() {
+	if(totalEMI==InsureAmount)
+	{
+	return InsureAmount;
+   }
+	else {
+		return totalEMI;
+	}
+
+
+}
+public void setInsureAmount(int InsureAmount)
+{
+	this.InsureAmount=InsureAmount;
+}
+public int GetInsureAmount()
+{
+	return InsureAmount;
+}
+String EmpCode;
+public void setecode(String EmpCode)
+{
+	this.EmpCode=EmpCode;
+}
+private String getecode() {
+	
+	// TODO Auto-generated method stub
+	return  EmpCode;
+}
+public void setemi(int totalEMI)
+{
+	this.totalEMI=totalEMI;
+	
+}
+public int getemi()
+{
+	return totalEMI;
+}
+	
+public static void main(String[] args) {
+
+	Employee emp=new Employee("John", "HR", 35, 200, 300, 15000);
+	emp.empdisplay();
+	emp.setecode("E200");
+	emp.setemi(50000);
+	emp.payEMI();
+	emp.setInsureAmount(3000);
+	emp.registerInsurance();
+	if(emp.totalEMI==emp.InsureAmount)
+	{
+		int insur=emp.claimInsurance();
+		System.out.println(" insureamount:"+insur);
+   }
+	else {
+		int total=emp.claimInsurance();
+		
+		System.out.println("totalEMI:"+total);
+	}
+}
+		
+	
+	
+	
+
+}
